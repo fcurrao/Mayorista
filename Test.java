ce01252b6d7c0b89568fd6323eaa2f514057a28a
fcurrao <68132577+fcurrao@users.noreply.github.com>
2021-05-31 13:31:43 -0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,24 @@
+
+public class Test {
+
+	@Test
+	public void testQueVerificaQueSeCreoLaFactura() {
+	
+	}
+
+	@Test
+	public void testQueVerificaQueSeAgregaUnItem(){
+		
+	}
+	@Test
+	public void testQueVerificaQueSePuedeAgregarMasDeUnItem(){
+
+	}
+	
+	
+	@Test
+	public void testQueVerificaQueSeAgregueElImpuestoCorrespondienteSegunElTipo(){
+	
+	}
+
+}
