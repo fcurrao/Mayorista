8590657c3cb2628fe84a732715906cc49ea31a40
fcurrao <68132577+fcurrao@users.noreply.github.com>
2021-05-31 13:39:02 -0300

    Update Factura.java

--- Factura.java
+++ Factura.java
@@ -1,11 +1,19 @@
 Public class Factura extends Mayorista {
+  private items=items;
   
-  public Factura(SUPER MAYORISTA){
-  SUPER
+  
+  public Factura(SUPER MAYORISTA , Items items){
+  this.items=items:
+    
     imprime mayorista
     imprime factura
     
+    
+        
+  public void agregarItems(Items items){
+    this.items.add(items);
      
+   }
     
  }
 }
